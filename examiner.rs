2ab1f58943b3e5c506c32a77c60f7c3e3e8425f2
Juan E <jeduardo.garcia@gmail.com>
2020-04-24 15:13:25 -0400

    Create examiner.rs

---
+++ examiner.rs
@@ -0,0 +1,42 @@
+// Controller which make the question of the exam.
+use std::io;
+// ArrayAnsw contains the answer registered
+// ArrayQuest deliver the question according the progress
+
+fn main() {
+    println!("Initiating Exam...!");
+    // Number of question and display
+    readFile();
+    
+    while () {
+    
+     println!("Please input your answer:");
+     
+    }
+    
+    
+    
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
+
+// Load the questions in a new array
+fn readFile() {
+  
+}
+
+fn recordAnsw(ParamAnswer){
+  ArrayAnsw[] = ParamAnswer
+  return 0;
+}
+
+fn assetAnswer(){
+  #match
+}
+
+}
